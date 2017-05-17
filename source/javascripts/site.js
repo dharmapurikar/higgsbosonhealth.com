b3b90979153101f5fd47f30de0d6ba2eb687ec57
Supravat <sahoosupravat8@gmail.com>
2017-05-17 20:29:56 +0530

    Changed the webpage UI

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -5,7 +5,6 @@
 
 $(document).ready(function() {
     $(window).scroll(function() {
-        console.log($(".navbar").offset().top)
         if ($(".navbar").offset().top > 50) {
             $(".navbar-fixed-top").addClass("top-nav-collapse");
         } else {
@@ -20,7 +19,7 @@ $(document).ready(function() {
                 $("html, body").animate({ scrollTop: $(document).height()-$(window).height() });
             } else {
                 $('html, body').stop().animate({
-                    scrollTop: $($anchor.attr('href')).offset().top
+                    scrollTop: $($anchor.attr('href')).offset().top - 40
                 }, 1500, 'easeInOutExpo');
             }
 
